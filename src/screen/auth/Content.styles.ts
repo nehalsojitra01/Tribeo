2bd205f64194b0f59eb9b4cbb90d7525f8249c5a
vibhuti0011 <vibhutisojitra@stallion-software.com>
2026-03-06 18:21:31 +0530

    v
    v

--- src/screen/auth/Content.styles.ts
+++ src/screen/auth/Content.styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet, Touchable, useWindowDimensions } from 'react-native';
 
 export const getStyles = (isDark: boolean) =>
     StyleSheet.create({
-        ContentContainer:{
+        MobileScreenContainer:{
             justifyContent:"center",
             alignContent:"center",
             alignItems:"center",
@@ -28,10 +28,9 @@ export const getStyles = (isDark: boolean) =>
           flex:1,
           backgroundColor: 'black'
         },
-
         title: {
             fontSize: 20,
-             },
+        },
          Icon:{
           flex:0.1,
           justifyContent:"center",
@@ -44,162 +43,84 @@ export const getStyles = (isDark: boolean) =>
             width:20,
             alignItems:"center",
         },
+        iconn:{
+            
+            backgroundColor:'#5ad40f',
+            borderRadius:20,
+            height:20,
+            width:20,
+            alignItems:"center",
+        },
         BackIcon:{
             flex:0.1,
             justifyContent:"center",
             alignItems:"center",
             padding:10
         },
-       
+           cardDark1: {
+            height: 5,
+            width: 350,
+            backgroundColor: '#323334',
+            marginTop: 20,
+            borderRadius:20,
+            alignContent:"center",
+            alignItems:"center"
+           },
+            cardDark3: {
+            height: 1,
+            width: 400,
+            backgroundColor: '#323334',
+            marginTop: 30,
+            borderRadius:20,
+           },
+        Txt:{
+            textAlign:"left",
+            alignContent:"flex-start",
+            alignItems:"flex-start",
+            fontSize:20,
+            fontFamily:"Quicksand-Bold",
+            color:"#fff",
+            padding:10
+        },
+          item: {
+            flexDirection:"row",
+            justifyContent:"flex-start",
+            backgroundColor: '#373434',
+            padding: 15,
+            marginVertical: 10,
+            marginHorizontal: 10,
+            borderRadius:10 , 
+            borderBottomWidth: 1,
+            borderBottomColor: '#363631',
+            alignItems:"center",
+            fontFamily:"Quicksand-Bold",
+        },
         Next:{
             color:"#ea1f7b",
             marginBottom:135,
             padding:5,
             fontFamily:"Quicksand-Bold",
         },
-        TEXT:{
-            fontFamily:"Quicksand-Bold",
-            fontSize:20,
-            justifyContent:"flex-start",
-            alignItems:"flex-start",
-            color:"#fff",
-            padding:20,
-            marginTop:-50
-        },
-        
-        container1: {
-            paddingHorizontal: 20,
+         Text:{
+          textAlign:"center",
+          fontSize:20,
+          fontFamily:"Quicksand-Bold",
+          margin:-10,
+          color:"#fff"
          },
-        input2: {
-            height: 50,
-            width:280,
-            backgroundColor:"#282C34",
-            borderRadius: 10,
-            padding:10,
-            fontSize: 18,
-            color: "#fff",
-            fontFamily:"Quicksand-Bold",
-        }, 
-        container2: {
-            paddingHorizontal: 30,
+
+        Touchable:{
+          backgroundColor: "#F63679",
+          
+          borderRadius:50, 
+          height:90,
+          maxHeight:50,
+          width:300,
+          margin:50,
+          fontSize:15
         },
-        plus:
-        {
-            padding:-30,
-            
+        Eye:{
+          margin:15,
+          alignItems:"flex-start"
         },
-        Txt:{
-            padding:5,
-            color:"#fff"
-            
-        },
-        container3: {
-            flexDirection:"row",
-            alignItems: "center",
-        },
-        dropdownButton: {
-           backgroundColor:"#282C34",
-            marginTop:-10,
-            borderRadius: 10,
-            padding:10,
-            height:50,
-            width:90,
-        },
-        buttonText: {
-            color: "#fff",
-            height:40,
-            padding:10,
-            fontSize: 16,
-        },
-        overlay: {
-            flex: 1,
-            justifyContent: "flex-end",
-            alignItems: "flex-end",
-            marginBottom:100,
-            borderColor:"#fcf8f8",
-        },
-        dropdown: {
-            backgroundColor:"#1d1e21",
-            width: 100,
-            borderRadius: 10,
-            paddingVertical: 20,
-        },
-        item: {
-            padding: 12,
-        },
-        itemText: {
-            color: "#fff",
-            fontSize: 16,
-        }, 
-         containerr: {
-           backgroundColor:"#282C34",
-            borderRadius: 10,
-            margin: 20,
-        },
-        picker: {
-        }, 
-        modalText1:{
-            padding:20,
-            fontFamily:"Quicksand-Bold",
-            color:"#fff"
-        },
-        modalOverlay:{
-            flex:1,
-            justifyContent:"flex-end",
-            backgroundColor:"rgba(4, 3, 3, 0.77)"
-            },
-        bottomModal:{
-            backgroundColor:"#1f232b",
-            borderTopLeftRadius:25,
-            borderTopRightRadius:25,
-            padding:20
-            },
-        line:{
-            height:4,
-            width:40,
-            backgroundColor:"#666",
-            alignSelf:"center",
-            borderRadius:5,
-            marginBottom:20
-            },
-        title:{
-            color:"#fff",
-            fontSize:18,
-            marginBottom:10,
-            fontFamily:"Quicksand-Bold"
-            },
-        input:{
-            backgroundColor:"#191919dd",
-            borderRadius:10,
-            padding:12,
-            color:"#fff",
-            marginTop:10
-            },
-        radioRow:{
-            flexDirection:"row",
-            alignItems:"center",
-            marginRight:20
-            },
-        radio:{
-            height:18,
-            width:18,
-            borderRadius:9,
-            borderWidth:2,
-            fontFamily:"Quicksand-Bold",
-            borderColor:"#fff",
-            marginRight:8
-            },
-        radioText:{
-            color:"#dccaca",
-            fontFamily:"Quicksand-Bold"
-            },
-        Btn:{
-            backgroundColor:"#d46487",
-            padding:15,
-            borderRadius:20,
-            alignItems:"center",
-            marginTop:20,
-            fontSize:20,
-            fontFamily:"Quicksand-Bold"
-        }
     });
--- src/screen/auth/Content.tsx
+++ src/screen/auth/Content.tsx
@@ -1,26 +1,115 @@
 import React, { useState } from 'react';
 import {Button, StyleSheet, Text, TextInput,View,Image, ScrollView, useWindowDimensions, TouchableOpacity, Modal, Alert, Pressable, FlatList, StatusBar, Touchable, ImageBackground, Dimensions, FlatListComponent} from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
-import { getStyles } from './Content.styles';
+import { getStyles } from './MobileSceen.styles';
 import { getThemeStylesForClass } from '../../helpers/themeHelper';
 import { ThemeContextType } from '../../component/ThemeContext';
+import Gallery from './../../assets/images/Gallery.svg';
+import Leave from './../../assets/images/leave.svg'
+import Pluse from '../../assets/images/pluse.svg'
+import Threedot from '../../assets/images/threedot.svg'
+import Eye from '../../assets/images/eye.svg'
 import TopHeader  from './../../component/TopHeader'
-import Plus from './../../assets/images/plus.svg'
-
-
-const CurrencyDropdown = () => {
-    const [selectedValue, setSelectedValue] = useState("Paid");
+const ContentScreen = () => {
     const context={} as ThemeContextType;
+    const [selected, setSelected] = useState('sms');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [modalVisible, setModalVisible] = useState(false);
     const { styles, isDark } = getThemeStylesForClass(context, getStyles);
+  
+    const DATA = [
+        {
+        title:"Content Name",
+        descriptions:"Draft",
+        },
+        {
+        title:"Published Content",
+        descriptions:"Published",
+        },
+        {    
+        title:"Scheduled Content",
+        descriptions:"Scheduled day0,10;00",
+        },
+    ];
+    type ItemProps = {title:string,descriptions:string};
+    const Item = ({title,descriptions,color}:ItemProps) => (
+        <View style={styles.item}>
+            <View style={{flex:0.1}}>
+                <View style={[styles.icon,{backgroundColor:color}]}>
+                     <Gallery resizeMode="contain" style={{height:50,with:50}}/>
+                </View>
+            </View>
+            <View style={{flex:0.8,alignItems:"flex-start",}}>
+                <Text style={{fontFamily:"Quicksand-Bold",color:"#f7f7f4",fontSize:17}}>{title}</Text>
+                <View style={{alignContent:"space-evenly"}}>
+                    <Text style={{fontFamily:"Quicksand-Bold",color:"#3a9c43",fontSize:14}}>{descriptions}</Text>
+                </View>
+             </View>
+                <View style={{flex:0.1}}>
+                    <View style={[styles.icon,{backgroundColor:color}]}>
+                        <TouchableOpacity>
+                            <Threedot resizeMode="contain" style={{height:50,with:50}}/>
+                        </TouchableOpacity>
+                    </View>
+                </View> 
+        </View>  
+    )
+   
   return (   
     <SafeAreaProvider>
         <SafeAreaView style={styles.container}>
             <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-            <TopHeader title={"Content Manager"}/>
-           
+           <TopHeader title={"Content Manager"}/>
+            <View style={styles.cardDark}></View>
+            <View style={styles.divider} /> 
+                <View style={{flexDirection:"row",height:70,backgroundColor:"#272627",borderRadius:17,marginTop:12,padding:12}}>
+                    <View style={{flexDirection:"row",justifyContent:"center"}}>
+                        <View style={styles.View}>
+                            <View style={{flex:0.1}}>
+                            <Pressable
+                                style={[styles.button,styles.buttonClose]}
+                                onPress={() => setModalVisible(true)}>
+                                    <TouchableOpacity>
+                                        <Pluse name="laptop-code" color="black" />
+                                    </TouchableOpacity>
+                            </Pressable>
+                            </View>
+                        </View>
+                    </View>
+                        <View style={{flex:0.8,justifyContent:"center"}}>
+                            <Text style={{color:"#ffF",marginBottom:20,padding:5}}>Create a New Product</Text> */}
+                        </View>
+                 </View>                       
+                    <FlatList
+                        data={DATA}
+                        renderItem={({ item }) => (
+                            <Item
+                                descriptions={item.descriptions}
+                                title={item.title}
+                                color={item.Color}
+                            />
+                        )}
+                     /> 
+                    <View style={{flexDirection:"row",justifyContent:"center",flex:1}}>
+                        <View style={{ flex: 1, justifyContent: "flex-end",height: 100}}> 
+                            <TouchableOpacity style={[styles.Touchable,{flexDirection:"row",backgroundColor:"#F63679",margin:10,borderRadius:10,padding:10}]}>
+                                <View style={{flex:0.3}}>
+                                    <View style={styles.Eye}>
+                                        <TouchableOpacity>
+                                            <Eye resizeMode="contain" /> 
+                                        </TouchableOpacity>           
+                                    </View>
+                                </View> 
+                                <View style={{flex:0.7}}>
+                                    <Text style={styles.Text}>Preview Content</Text>
+                                </View>
+                            </TouchableOpacity>
+                            </View>
+                        </View>  
+              
   </SafeAreaView>
 </SafeAreaProvider>
     );  
 };
 
-export default CurrencyDropdown;
+export default ContentScreen;
